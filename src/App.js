57246a95a58dbd427d0b98a61403278b173e7cf0
ximocasanova <ximo.casanova.alabau@accenture.com>
2023-03-09 12:20:44 +0100

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/api/shorten', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ originalUrl: url }),
+      });
+
+      const data = await response.json();
+      setShortUrl(data.shortUrl);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setUrl(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Acortador de URLs</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Introduce la URL que deseas acortar:
+          <input type="text" value={url} onChange={handleChange} />
+        </label>
+        <button type="submit">Acortar URL</button>
+      </form>
+      {shortUrl && (
+        <div>
+          <p>La URL acortada es:</p>
+          <a href={shortUrl}>{shortUrl}</a>
+        </div>
+      )}
     </div>
   );
 }
