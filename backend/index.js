2fe8872aff12fd1892b96ab0e0946a5f57bc015b
ximocasanova <ximo.casanova.alabau@accenture.com>
2023-03-22 10:44:31 +0100

    first commit

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const shortid = require("shortid");
+const validUrl = require("valid-url");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const urlRoutes = require("./routes/urls");
+require("dotenv").config();
 
 const app = express();
 
@@ -20,7 +21,52 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch((error) => console.error(error));
 
-app.use("", urlRoutes);
+const urlSchema = new mongoose.Schema({
+  id: String,
+  url: String,
+  shortUrl: String,
+});
+
+const Url = mongoose.model("Url", urlSchema);
+
+// Ruta para crear una URL acortada
+app.post("/api/shorten", async (req, res) => {
+  const { url } = req.body;
+
+  // Ruta para manejar errores 400 (errores de los clientes)
+  // Error 400: Bad Request (sintaxis inválida)
+  if (!validUrl.isUri(url)) {
+    return res.status(400).json({ error: "Invalid URL" });
+  }
+
+  const id = shortid.generate();
+  const shortUrl = `http://localhost:3000/${id}`;
+
+  const newUrl = new Url({ id, url, shortUrl });
+  await newUrl.save();
+
+  return res.status(201).json({ id, shortUrl });
+});
+
+// Ruta para redirigir a la URL original a partir de la URL acortada
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const urlData = await Url.findOne({ id });
+
+  // Ruta para manejar errores 400 (errores de los clientes)
+  // Error 404: Not Found (no pudo encontrar el contenido solicitado, URL no válida)
+  if (!urlData) {
+    return res.status(404).json({ error: "URL not found" });
+  }
+
+  return res.redirect(urlData.url);
+});
+
+// Ruta para manejar errores 500 (errores de los servidores)
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).json({ error: "Internal Server error" });
+});
 
 const port = process.env.PORT || 5000;
 
